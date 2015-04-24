f0657452d7af0fae160e58a5fdfe7aebca338035
Roger Jin <yangzhen.jin@hp.com>
2015-04-24 14:19:56 +0800

    add nginx reverse proxy conf

--- Brocfile.js
+++ Brocfile.js
@@ -1,8 +1,6 @@
 'use strict';
 
 var fs = require('fs-extra'),
-    path = require('path'),
-    symlinkOrCopySync = require('symlink-or-copy').sync,
     mergeTree = require('broccoli-merge-trees'),
     concatAndSourceMap = require('broccoli-sourcemap-concat'),
     compileLess = require('broccoli-less-single');
@@ -12,53 +10,14 @@ var adminCss, sawCss, sapCss;
 
 function cb(err) {
     if (err) return console.error(err);
-    console.log('Succeed');
+    console.log('.....copy in success....');
 }
 
-fs.copy('broccoli/css-files-include.html', '../ui/app/css-files-include.html', cb);
-fs.copy('broccoli/js-files-include.html', '../ui/app/js-files-include.html', cb);
-
-function isMergeTreeExists(filename){
-    try{
-        fs.lstatSync(filename);
-        return true;
-    }
-    catch (err){
-        return false;
-    }
-}
-
-fs.watch('tmp', function (event, filename) {
-
-    if (filename && filename.indexOf('tree_merge') === 0) {
-        var start = process.hrtime();
-        console.log(event + filename);
-        if (isMergeTreeExists('tmp' + path.sep + filename)) {
-            console.log('find a valid resulting merged tree -' + filename);
-            fs.lstat('../ui/app/devjs', function (err, lstat) {
-
-                if (!err && lstat.isSymbolicLink()) {
-                    //link exists, remove at first
-                    fs.unlinkSync('../ui/app/devjs');
-                }
-                else if(!err) {
-                    fs.removeSync('../ui/app/devjs');
-                }
-
-                //link broccoli tree output to maas link devjs
-                symlinkOrCopySync('tmp' + path.sep + filename, '../ui/app/devjs');
-
-                var diff = process.hrtime(start);
-
-                console.log('benchmark took %d nanoseconds', diff[0] * 1e9 + diff[1]);
-            })
-        }
-    }
-});
-
+fs.copy('html/css-files-include.html', '../ui/app/css-files-include.html', cb);
+fs.copy('html/js-files-include.html', '../ui/app/js-files-include.html', cb);
+fs.copy('nginx_conf/maas-locations.conf', '../tomcats/nginx/conf/maas-locations.conf', cb);
 
 // organize dev stuffs into tree
-
 adminCss = compileLess('../ui/app/js/modules/admin', 'resources/css/theme.less', 'style/admin/theme.css');
 sawCss = compileLess('../ui/app/js/modules/saw', 'resources/css/theme.less', 'style/saw/theme.css');
 sapCss = compileLess('../ui/app/js/modules/sap', 'resources/css/theme.less', 'style/sap/theme.css');
