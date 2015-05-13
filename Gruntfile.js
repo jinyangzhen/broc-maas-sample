60e50b331e747de84d874307c33b1e4670b70377
Roger Jin <yangzhen.jin@hp.com>
2015-05-13 11:28:23 +0800

    change to use Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,24 @@
 module.exports = function (grunt) {
 
-    require('time-grunt')(grunt);
+    var fs = require('fs-extra');
+
+    function cb(err) {
+        if (err) return console.error(err);
+        console.log('.....copy in success....');
+    }
+
+    fs.copy('html/css-files-include.html', '../ui/app/css-files-include.html', cb);
+    fs.copy('html/js-files-include.html', '../ui/app/js-files-include.html', cb);
 
     grunt.initConfig({
             less:{
                 dev:{
                     options: {
-                        paths: ['../ui/app/resources','../ui/app/js/modules'],
-                        sourceMap: true
+                        paths: ['../ui/app/resources','../ui/app/js/modules']//,
+                       // sourceMap: true
                     },
                     files: {
-                        '../ui/app/devcss/style/plat/theme.css': '../ui/app/resources/css/theme.less',
+                        '../ui/app/resources/css/theme.css': '../ui/app/resources/css/theme.less',
                         '../ui/app/devcss/style/admin/theme.css': '../ui/app/js/modules/admin/resources/css/theme.less',
                         '../ui/app/devcss/style/sap/theme.css': '../ui/app/js/modules/sap/resources/css/theme.less',
                         '../ui/app/devcss/style/saw/theme.css': '../ui/app/js/modules/saw/resources/css/theme.less'
@@ -23,32 +31,32 @@ module.exports = function (grunt) {
                 },
                 admin: {
                     files: {
-                        '../ui/app/devjs/admin.js': ['../ui/app/js/modules/admin/admin.js', '../ui/app/modules/admin/**/*.js']
+                        '../ui/app/devjs/admin.js': ['../ui/app/js/modules/admin/admin.js', '../ui/app/js/modules/admin/**/*.js']
                     }
                 },
                 infra: {
                     files: {
-                        '../ui/app/devjs/infra.js': ['../ui/app/modules/infra/**/*-module.js', '../ui/app/modules/infra/**/*.js']
+                        '../ui/app/devjs/infra.js': ['../ui/app/js/modules/infra/**/*-module.js', '../ui/app/js/modules/infra/**/*.js']
                     }
                 },
                 shared: {
                     files: {
-                        '../ui/app/devjs/shared.js': ['../ui/app/modules/shared/shared.js', '../ui/app/modules/shared/**/*.js']
+                        '../ui/app/devjs/shared.js': ['../ui/app/js/modules/shared/shared.js', '../ui/app/js/modules/shared/**/*.js']
                     }
                 },
                 saw: {
                     files: {
-                        '../ui/app/devjs/saw.js': ['../ui/app/modules/saw/saw.js', '../ui/app/modules/saw/**/*.js']
+                        '../ui/app/devjs/saw.js': ['../ui/app/js/modules/saw/saw.js', '../ui/app/js/modules/saw/**/*.js']
                     }
                 },
                 platform: {
                     files: {
-                        '../ui/app/devjs/platform.js': ['../ui/app/modules/platform/**/*-module.js', '../ui/app/modules/platform/**/*.js']
+                        '../ui/app/devjs/platform.js': ['../ui/app/js/modules/platform/**/*-module.js', '../ui/app/js/modules/platform/**/*.js']
                     }
                 },
                 sap: {
                     files: {
-                        '../ui/app/devjs/sap.js': ['../ui/app/modules/sap/sap.js', '../ui/app/modules/sap/**/*.js']
+                        '../ui/app/devjs/sap.js': ['../ui/app/js/modules/sap/sap.js', '../ui/app/js/modules/sap/**/*.js']
                     }
                 }
             },
@@ -58,6 +66,7 @@ module.exports = function (grunt) {
                     files: ['../ui/app/js/modules/saw/**/*.js'],
                     tasks: ['concat_sourcemap:saw'],
                     options: {
+                        interval:5007,
                         spawn: false
                     }
                 },
@@ -65,6 +74,7 @@ module.exports = function (grunt) {
                     files: ['../ui/app/js/modules/platform/**/*.js'],
                     tasks: ['concat_sourcemap:platform'],
                     options: {
+                        interval:5007,
                         spawn: false
                     }
                 },
@@ -72,6 +82,7 @@ module.exports = function (grunt) {
                     files: ['../ui/app/js/modules/infra/**/*.js'],
                     tasks: ['concat_sourcemap:infra'],
                     options: {
+                        interval:5007,
                         spawn: false
                     }
                 },
@@ -79,6 +90,7 @@ module.exports = function (grunt) {
                     files: ['../ui/app/js/modules/shared/**/*.js'],
                     tasks: ['concat_sourcemap:shared'],
                     options: {
+                        interval:5007,
                         spawn: false
                     }
                 },
@@ -86,6 +98,7 @@ module.exports = function (grunt) {
                     files: ['../ui/app/js/modules/sap/**/*.js'],
                     tasks: ['concat_sourcemap:sap'],
                     options: {
+                        interval:5007,
                         spawn: false
                     }
                 },
@@ -93,6 +106,7 @@ module.exports = function (grunt) {
                     files: ['../ui/app/js/modules/admin/**/*.js'],
                     tasks: ['concat_sourcemap:admin'],
                     options: {
+                        interval:5007,
                         spawn: false
                     }
                 },
@@ -100,6 +114,7 @@ module.exports = function (grunt) {
                     files: ['../ui/app/**/*.less'],
                     tasks: ['less:dev'],
                     options: {
+                        interval:5007,
                         spawn: false
                     }
                 }
@@ -112,11 +127,12 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
 
-    grunt.registerTask('default', ['concat_sourcemap:admin',
+    grunt.registerTask('default', ['less:dev','concat_sourcemap:admin',
         'concat_sourcemap:infra',
         'concat_sourcemap:shared',
         'concat_sourcemap:saw',
         'concat_sourcemap:platform',
-        'concat_sourcemap:sap'
+        'concat_sourcemap:sap',
+        'watch'
     ]);
 };
